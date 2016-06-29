13abe22ff6798b3a3017ca5ae200a90ca3a6cc12
silent6910 <mailforjob6910@gmail.com>
2016-06-29 09:32:48 +0800

    Add files via upload

---
+++ homework 1 html .php.php
@@ -0,0 +1,20 @@
+
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<body>
+   <style>
+   th {color:red;background-color:#1E90FF;border-style:dashed;} 
+   caption{color:blue;font-family:DFKai-sb;border-style:dashed;background-color:#77FF00}
+   tr{background-color:#FF8900;color:#F7FF00;border-style:dotted;}
+   </style>
+    <p style='border: 5px solid green; '>
+    <table border="1" width="100%" > 
+        <caption>段考成續單</caption> 
+        <tbody><tr><th  rowspan="2">姓名</th><th colspan="3">各科成績</th><th rowspan="2">名次 
+        </th></tr><tr><th>Algorithm</th><th>Data structure</th><th>Programming 
+        </th></tr><tr align="center"><td>阿一</td><td>78</td><td>80</td><td>90</td><td>1</td></tr>
+        <tr align="center"><td>小二</td><td>76</td><td>64</td><td>80</td><td>2</td></tr>
+        <tr align="center"><td>大三</td><td>60</td><td>50</td><td>80</td><td>3</td></tr>
+    </tbody></table>
+</p> 
+
+</body>
